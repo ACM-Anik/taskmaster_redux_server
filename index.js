d045f7e223074a63881caa95436ad577ba84594d
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-14 23:17:51 +0600

    MongoDB connected properly and some changes done

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-const uri = process.env.DATABASE_URI;
+const uri = "mongodb+srv://taskmaster_redux:TeffZWBBpqaxdrIX@cluster0.vxma4ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const uri = process.env.DATABASE_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,6 +27,10 @@ async function run() {
 
     console.log('Successfully connected to MongoDB!');
 
+    app.get('/', (req, res) => {
+      res.send('Task Master Server');
+    });
+
    
   } finally {
   }
@@ -32,5 +38,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Taskmaster server listening on port ${port}`);
 });
